e58591020de2aa320fea0a2ee68cc04a22cffe3c
Aliaksei Kuncevic <kncvch@gmail.com>
2017-11-16 16:10:10 +1100

    remove outdated http module refs, fix linting error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { Feathers } from './services/feathers.service';
@@ -21,7 +20,6 @@ import { AuthGuard } from './guards/auth.guard';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     AppRoutingModule
   ],
   providers: [
