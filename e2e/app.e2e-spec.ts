cffbeb9c4de3aa6cd318acb1bf5f39ab02a7c3d5
Jan Lohage <j2L4e@users.noreply.github.com>
2018-02-05 23:26:54 +0100

    update to feathers buzzard and angular 5.2.0, added legacy 'feathers' typings stub (#9)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ponyracer App', () => {
+describe('feathers-chat-angular App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,12 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+// tslint:disable
+// feathers-reactive's bundled typings still augment module 'feathers' (auk). This stub keeps TS from complaining.
+// todo: remove when feathers-reactive typings are up-to-date with buzzard
+declare module 'feathers' {
+  interface NullableId {}
+  interface Pagination {}
+  interface Params {}
+}
